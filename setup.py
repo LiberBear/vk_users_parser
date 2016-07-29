a8f6eb9051fde0a4a8f63e85a67387e4dc6d9c4f
White Bear <mihailfs@yandex.ru>
2016-07-29 11:13:10 +0300

    travis

--- setup.py
+++ setup.py
@@ -1,8 +1,8 @@
 from cx_Freeze import setup, Executable
 
 setup(
-    name = vk_users_parser,
-    version = 0.1,
-    description = Parsing users from groups,
+    name = 'vk_users_parser',
+    version = '0.1',
+    description = 'Parsing users from groups',
     executables = [Executable(vk_users_parser.py)]
 )
