a268fb31a423fb8d44acf5e3adba996583359354
White Bear <mihailfs@yandex.ru>
2016-07-29 11:10:41 +0300

    travis

---
+++ setup.py
@@ -0,0 +1,8 @@
+from cx_Freeze import setup, Executable
+
+setup(
+    name = vk_users_parser,
+    version = 0.1,
+    description = Parsing users from groups,
+    executables = [Executable(vk_users_parser.py)]
+)
